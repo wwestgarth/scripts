d65f2c28b5122784f4b8888a0bb49ce8b3f52a13
wwestgarth <william@vega.xyz>
2022-01-12 16:57:47 +0000

    MOAR

--- node-startup/init-vega.sh
+++ node-startup/init-vega.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
+
+set -ex
+
 HERE="$(dirname "$(realpath "$0")")"
 
 docker run -t -d --rm -v "${HERE}"/vegahome:/vegahome --entrypoint=/bin/bash --name core docker.pkg.github.com/vegaprotocol/vega/vega:local
 
 # Init core and faucet
-docker exec core vega init -f --home=/vegahome --nodewallet-passphrase-file=/vegahome/passphrase-file --output=json
-
-# Make the nodewallets
-docker exec core vega nodewallet generate --passphrase-file=/vegahome/passphrase-file --wallet-passphrase-file=/vegahome/passphrase-file --chain=vega --home=/vegahome  --output=json
-docker exec core vega nodewallet generate --passphrase-file=/vegahome/passphrase-file --wallet-passphrase-file=/vegahome/passphrase-file --chain=ethereum --home=/vegahome --output=json
-
-# copy good config and do faucet
-cp "${HERE}"/vegahome/config.toml "${HERE}"/vegahome/config/node
-docker exec core vega faucet init -f --home=/vegahome --update-in-place --passphrase-file=/vegahome/passphrase-file --output=json
+#docker exec core vega init -f validator --home=/vegahome --nodewallet-passphrase-file=/vegahome/passphrase-file --output=json
 
 # genesis file creation and update with appstate
-docker exec core vega tm init --home=/vegahome
-docker exec core vega genesis generate --home=/vegahome --passphrase-file=/vegahome/passphrase-file
-docker exec core vega genesis update --tm-root=/vegahome --home=/vegahome --passphrase-file=/vegahome/passphrase-file
+docker exec core vega tm init  
+
+# Make the nodewallets
+#docker exec core vega nodewallet generate --force --passphrase-file=/vegahome/passphrase-file --wallet-passphrase-file=/vegahome/passphrase-file --chain=vega --home=/vegahome  --output=json
+#docker exec core vega nodewallet generate --force --passphrase-file=/vegahome/passphrase-file --wallet-passphrase-file=/vegahome/passphrase-file --chain=ethereum --home=/vegahome --output=json
+#docker exec core vega nodewallet import --output=json --passphrase-file=/vegahome/passphrase-file --wallet-passphrase-file=/vegahome/passphrase-file --chain=tendermint --tendermint-home=/vegahome --home=/vegahome 
+
+# copy good config and do faucet
+cp "${HERE}"/config.toml "${HERE}"/vegahome/config/node
+docker exec core vega faucet init -f --home=/vegahome --update-in-place --passphrase-file=/vegahome/passphrase-file --output=json
+
+
+docker exec core vega genesis generate --home=/vegahome --passphrase-file=/vegahome/passphrase-file --tm-home=/vegahome
+docker exec core vega genesis update --tm-home=/vegahome --home=/vegahome --passphrase-file=/vegahome/passphrase-file --tm-home=/vegahome
+
+
+
 
 docker kill core
 
